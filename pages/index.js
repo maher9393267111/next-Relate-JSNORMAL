bb5b00e29ec7d3fee40f6e743a3db8d6acf81cb1
maher <gomemahero.com>
2022-10-10 18:25:56 +0300

    fix class

--- pages/index.js
+++ pages/index.js
@@ -6,13 +6,13 @@ const HomeScreen = () => {
 
      <h1>asas</h1>
 
-     <div class="mobile-menu-customer-support">
-                    <div class="mobile-menu-customer-support-icon">
+     <div className="mobile-menu-customer-support">
+                    <div className="mobile-menu-customer-support-icon">
                         <img src="assets/images/icon/support-icon.png" alt=""/>
                     </div>
                     <div class="mobile-menu-customer-support-text">
                         <span>Customer Support</span>
-                        <a class="mobile-menu-customer-support-text-phone" href="tel:(08)123456789">(08) 123 456 789</a>
+                        <a className="mobile-menu-customer-support-text-phone" href="tel:(08)123456789">(08) 123 456 789</a>
                     </div>
                 </div>
 
